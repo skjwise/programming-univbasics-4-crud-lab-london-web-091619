c9fc64637a2d17a70ec338b661f84319a3aace5a
Sarah Wise <skjwise@hotmail.com>
2019-08-21 17:40:03 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,43 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  [2, 6, 7, 0]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  words = ["hi", "sit", "nose", "laugh"]
+  words.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  mix = [3, "zoom", "pow", 12]
+  mix.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  words = ["hi", "sit", "nose", "arrays!"]
+  words.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  mix = ["wow", "smile", 5, 4]
+  mix.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array[4] = "totally"
 end
